2f4b84d86bebb78e5e41f61041fb87dc4f232caf
SACHIN <98344845+Sachin24063@users.noreply.github.com>
2023-02-09 22:07:55 +0530

    Update 15. Maximum Compatibility Score Sum.cpp

--- Challenge company 5 : Google/15. Maximum Compatibility Score Sum.cpp
+++ Challenge company 5 : Google/15. Maximum Compatibility Score Sum.cpp
@@ -1,32 +1,26 @@
 	class Solution {
 	public:
-    int solve(int i,
-    vector<vector<int>>&students,vector<vector<int>>&mentors,vector<int>&visited)
-    {
-        if(i>=students.size())
-            return 0;
-        
-        int maxi=0;
-        for(int j=0;j<mentors.size();j++){
-            if(visited[j]==0){
-                visited[j]=1;
-                int match=0;
-                for(int k=0;k<students[i].size();k++){
-                    if(students[i][k]==mentors[j][k])
-                        match++;
+    int solve(int idx,vector<vector<int>>& students, vector<vector<int>>&mentors,vector<bool>& visited){
+        if(idx>=students.size()) return 0;
+        int max_ans = 0;
+        for(int i=0;i<mentors.size();i++){
+            if(visited[i]==true){
+                visited[i] = false;
+                int temp = 0;
+                for(int j=0;j<students[idx].size();j++){
+                    if(students[idx][j] == mentors[i][j])
+                        temp++;
                 }
-                int score= match + solve(i+1,students,mentors,visited);
-                maxi=max(maxi,score);
-                visited[j]=0;
+                int max_score = temp+solve(idx+1,students,mentors,visited);
+                        max_ans = max(max_ans, max_score);
+                         visited[i] = true;s
             }
         }
-        return maxi;
+        return max_ans;
     }
-    
     int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
-        
         int m=students.size();
-        vector<int>visited(m,0);
+        vector<bool>visited(m,true);
         return solve(0,students,mentors,visited);
     }
 };
